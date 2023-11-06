5f4e0626043eef8eeb0954ac43d55c3e30b19647
AntoninVorain <antoninvorain@gmail.com>
2023-11-06 09:50:47 +0100

    installation front et back

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+// const databaseUrl =
+//   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const databaseUrl = process.env.DATABASE_URL
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(`${process.env.DATABASE_URL}`, { useNewUrlParser: true, useUnifiedTopology: "true" })
+    // await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: "true" })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
